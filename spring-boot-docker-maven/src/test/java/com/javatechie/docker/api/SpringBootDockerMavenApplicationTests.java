75bc23aa9d9ffe390ac00b06c3e30316f31be785
Joseph Haddison <joseph@edwiki.in>
2024-10-13 12:18:21 -0400

    Newly Added Files

--- spring-boot-docker-maven/src/test/java/com/javatechie/docker/api/SpringBootDockerMavenApplicationTests.java
+++ spring-boot-docker-maven/src/test/java/com/javatechie/docker/api/SpringBootDockerMavenApplicationTests.java
@@ -1,13 +1,21 @@
 package com.javatechie.docker.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class SpringBootDockerMavenApplicationTests {
+@SpringBootApplication
+@RestController
+public class SpringBootDockerMavenApplication {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerMavenApplication.class, args);
+    }
 
+    @GetMapping("/get")
+    public String getMessage() {
+        return "Lord Thank You For Blessing My Skills. Your Humble Servant Joseph Haddison";
+    }
 }
+
